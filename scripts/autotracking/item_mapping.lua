e1b62fbdae2e8d3a821bffdbf1d5c55bdc795381
NiwatoriCode <213150313+NiwatoriCode@users.noreply.github.com>
2025-11-02 02:29:08 -0500

    added map locations up to egg utopia

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -3,11 +3,9 @@
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
 BASE_ITEM_ID = 0
 ITEM_MAPPING = {
-	[BASE_ITEM_ID + 00000] = { { "toggle" } },
-	[BASE_ITEM_ID + 00001] = { { "progressive" } },
-	[BASE_ITEM_ID + 00002] = { { "consumable" } },
-	-- handle progressive_toggle as toggle, only changing it's active state
-	[BASE_ITEM_ID + 00003] = { { "progressive_toggle", "toggle" } },
-	-- multiple items on this id, add the consumable 3 times
-	[BASE_ITEM_ID + 00004] = { { "toggle" }, { "consumable", nil, 3 } }
+	[100] = { { "sonic" } },
+	[101] = { { "cream" } },
+	[102] = { { "tails" } },
+	[103] = { { "knuckles" } },
+	[104] = { { "amy" } }
 }
